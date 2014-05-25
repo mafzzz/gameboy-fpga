84d7a3976d44c966b3f14f844ad89732dc768c89
Sohil Shah <sohils@cmu.edu>
2014-05-25 10:54:46 -0400

    Added license file and copyright descriptions

--- constants.sv
+++ constants.sv
@@ -1,3 +1,24 @@
+/**************************************************************************
+*	"constants.sv"
+*	GameBoy SystemVerilog reverse engineering project.
+*   Copyright (C) 2014 Sohil Shah
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.
+*	
+*	Contact Sohil Shah at sohils@cmu.edu for information or questions. 
+**************************************************************************/
+
 `define TRUE 	1'b1
 `define	FALSE 	1'b0
 
--- datapath.sv
+++ datapath.sv
@@ -1,3 +1,24 @@
+/**************************************************************************
+*	"datapath.sv"
+*	GameBoy SystemVerilog reverse engineering project.
+*   Copyright (C) 2014 Sohil Shah
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.
+*	
+*	Contact Sohil Shah at sohils@cmu.edu for information or questions. 
+**************************************************************************/
+
 `include "constants.sv"
 `include "registerfile.sv"
 `include "alu.sv"
