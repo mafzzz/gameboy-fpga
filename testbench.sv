562f820e228509824aa0b0fa432896fed6a4e28a
Sohil Shah <sohils@cmu.edu>
2014-05-25 22:15:04 -0400

    [ALU] Added testing for ALU

--- testbench.sv
+++ testbench.sv
@@ -26,3 +26,106 @@
 *	WIP
 *
 */
+module testbench();
+
+	logic [7:0] 	A, B, result;
+	alu_op_t		op_code;
+	logic [3:0]		currFlags, newFlags;
+	logic [15:0]	addr;
+	
+	alu DUT(.op_A(A), .op_B(B), .alu_result(result), .curr_flags(currFlags), .next_flags(newFlags), .addr_result(addr), .op_code(op_code));
+	
+	initial begin
+		$monitor("%h	%s		%h	=	%h		flags {Z N H C} = [%b -> %b]", A, op_code.name, B, result, currFlags, newFlags);
+		
+		A <= '0; op_code <= alu_NOP;
+		B <= '0; currFlags <= '0;
+		#10;
+		
+		A <= 8'hff;
+		B <= 8'hff;
+		op_code <= alu_ADD;
+		currFlags <= newFlags;
+		#10;
+		
+		A <= 8'h00;
+		B <= 8'hff;
+		op_code <= alu_SUB;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'h5;
+		B <= 8'hf6;
+		op_code <= alu_RRC;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'h7;
+		B <= 8'hff;
+		op_code <= alu_RL;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'h9;
+		B <= 8'h0;
+		op_code <= alu_SLA;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'hB;
+		B <= 8'hBC;
+		op_code <= alu_SRL;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'h12;
+		B <= 8'h34;
+		op_code <= alu_CPL;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'hAA;
+		B <= 8'h5A;
+		op_code <= alu_XOR;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'hBB;
+		B <= 8'hC2;
+		op_code <= alu_NOP;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'hB1;
+		B <= 8'h40;
+		op_code <= alu_NOP;
+		currFlags <= newFlags;
+		#10;
+
+		A <= 8'h2B;
+		B <= 8'h1C;
+		op_code <= alu_ADC;
+		currFlags <= newFlags;
+		#10;
+		
+		#20;
+		$stop;
+	end
+
+endmodule: testbench
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
